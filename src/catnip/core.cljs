25105b588de04f6f0f26cfc7cc39920af4e28883
WadeGulbrandsen <wade.gulbrandsen@gmail.com>
2016-04-07 13:15:22 -0400

    Added fullscreen mode

--- src/catnip/core.cljs
+++ src/catnip/core.cljs
@@ -1,21 +1,25 @@
 (ns catnip.core
   (:require [reagent.core :as r :refer [atom]]
             [reagent.session :as session]
-            [secretary.core :as secretary :include-macros true]))
+            [secretary.core :as secretary :include-macros true]
+            [catnip.fullscreen :as fullscreen]))
 
 ;; -------------------------
 ;; State information
 
 ;; options is:
-;; {:max Int :speed Int :showui Bool :bgimg String}
-;;    - :max    is the maxiumum number of birds that can be on screen at a time
-;;    - :speed  is a scaling factor for the spped the birds fly
-;;    - :showui is a flag for determining if the UI elements should be shown or not
-;;    - :bgimg  is the path to the background image
-(def options (r/atom {:max 2 :speed 3 :showui true :bgimg ""}))
+;; {:max Int :speed Int :showui Bool :bgimg String :bgloaded Bool}
+;;    - :max      is the maxiumum number of birds that can be on screen at a time
+;;    - :speed    is a scaling factor for the spped the birds fly
+;;    - :showui   is a flag for determining if the UI elements should be shown or not
+;;    - :bgimg    is the path to the background image
+;;    - :bgloaded lets the program know if the browser has finished loading the bgimg yet
+(defonce options (r/atom {:max      2 :speed 3 :showui true
+                          :bgimg    "bg/nature-forest-waterfall-jungle.jpg"
+                          :bgloaded false}))
 
 ;; Keep a timestamp of the last time the screen was rendered
-(def last-ts (r/atom nil))
+(defonce last-ts (r/atom nil))
 
 ;; birds is (vectorOf Bird)
 ;; a Bird is:
@@ -26,19 +30,33 @@
 ;;    - :dx    is the speed of the bird on the X axis
 ;;    - :dy    is the speed of the bird on the Y axis
 ;;    - :frame is the frame of the sprite that should be shown
-(def birds (r/atom []))
+(defonce birds (r/atom []))
 
 ;; bird-sprite is:
-;; {:img String :width Int :height Int :frames Int}
+;; {:img String :width Int :height Int :frames Int :loaded Bool}
 ;;    - :img    is the path to the the sprite sheet
 ;;    - :width  is the width of the sprite
 ;;    - :height is the height of the sprite
 ;;    - :frames is the number of fames in the sprite sheet
-(def bird-sprite (r/atom {:img   "sprite/blue-jay-sprite-sheet.png"
-                          :width 100 :height 100 :frames 8}))
+;;    - :loaded lets the program know if the img is loaded yet
+(defonce bird-sprite (r/atom {:img   "sprite/blue-jay-sprite-sheet.png"
+                              :width 100 :height 100 :frames 8 :loaded false}))
 
 ;; -------------------------
 ;; Reagent components
+
+;; [] -> Component
+;; produce the loading box at the center of the screen
+(defn splash-ui []
+  [:div.splash
+   [:div.ui "Catnip"
+    [:div "Loading"
+     [:img {:src   (:img @bird-sprite)
+            :style {:display "none"}
+            :load  (swap! bird-sprite assoc :loaded true)}]
+     [:img {:src   (:bgimg @options)
+            :style {:display "none"}
+            :load  (swap! options assoc :bgloaded true)}]]]])
 
 ;; Bird -> Component
 ;; produce a div positioned at the bird's x and y co-ordinates. Inside the div is an img showing the current frame
@@ -61,8 +79,8 @@
     [:div {:style {:position  "fixed"
                    :top       (:y bird)
                    :left      (:x bird)
-                   :transform (str "rotate(" (str rotate) "deg)")
-                   }}
+                   :transform (str "rotate(" (str rotate) "deg)")}}
+
      [:img {:style {:position  "absolute"
                     :clip      (str "rect(0px, " (str offset-max) "px, "
                                     (str (:height @bird-sprite)) "px, " (str offset-min) "px)")
@@ -81,26 +99,35 @@
 ;; [] -> Component
 ;; produce the UI for the program
 (defn catnip-ui []
-  [:div (doall (map bird-component @birds))
-   [:div {:class "bottom"}
-    [:button {:style    {:position "fixed"
-                         :bottom   10
-                         :right    10
-                         :width    100}
-              :class    "ui"
-              :on-click #(swap! options assoc :showui (not (:showui @options)))}
+  [:div.bg {:style {:backgroundImage (str "url(\"" (:bgimg @options) "\")")}}
+   [:div (doall (map bird-component @birds))]
+   [:div.bottom
+    [:button.ui {:style    {:position "fixed"
+                            :bottom   10
+                            :left    10
+                            :width    100}
+                 :on-click #(swap! options assoc :showui (not (:showui @options)))}
      (if (:showui @options)
        "Hide options"
        "Show options")]
-    [:table {:class "options ui"
-             :style {:visibility (if (:showui @options)
-                                   "visible"
-                                   "hidden")}}
+    [:button.ui {:style    {:position "fixed"
+                            :bottom   10
+                            :right     10
+                            :width    100}
+                 :on-click (fn [e]
+                             (.preventDefault e)
+                             (fullscreen/toggle (-> e .-currentTarget .-parentNode .-parentNode)))}
+     (if (fullscreen/is-fullscreen?)
+       "Exit fullscreen"
+       "Go fullscreen")]
+    [:table.options.ui {
+                        :style {:visibility (if (:showui @options)
+                                              "visible"
+                                              "hidden")}}
      [:tr
-      [:td [:strong "Options"]]
-      [:td {:col-span 2
-            :style    {:text-align "right"}}
-       "Press F11 for fullscreen"]]
+      [:td {:col-span 3
+            :style    {:text-align "center"}}
+       "Options"]]
      [:tr
       [:td "Birds: "]
       [:td [slider options :max (:max @options) 1 10]]
@@ -108,9 +135,9 @@
      [:tr
       [:td [:div "Speed: "]
        [:td [slider options :speed (:speed @options) 1 10]]
-       [:td (:speed @options)]]
-      ]]]]
-  )
+       [:td (:speed @options)]]]]]])
+
+
 
 ;; -------------------------
 ;; Functions
@@ -133,12 +160,17 @@
       (> (:y bird) (:bottom bounds)) true
       :else false)))
 
-;; String -> []
-;; Updates the bgimg of the options and sets the body's background image to that image
+;;; [] -> []
+;;; Updates the background image form the one stored in options
+(defn apply-bg []
+  (set! (.-backgroundImage (.-style (.-body js/document))) (str "url(\"" (:bgimg @options) "\")")))
+
+;;; String -> []
+;;; Updates the bgimg of the options and sets the body's background image to that image
 (defn set-bg! [imgsrc]
   (do
     (swap! options assoc :bgimg imgsrc)
-    (set! (.-backgroundImage (.-style (.-body js/document))) (str "url(\"" imgsrc "\")"))))
+    (apply-bg)))
 
 ;; Int Bird -> Bird
 ;; produce a new bird with a new position and frame number based on the old bird and the delta of time since the last
@@ -195,6 +227,20 @@
   (reset! last-ts ts)
   (. js/window (requestAnimationFrame update!)))
 
+;; [] -> []
+;; Shows the loading box until the images are loaded
+;; after the images are done loading:
+;;   1. show the background image
+;;   2. create the initial batch of birds
+;;   3. render the normal ui
+;;   4. start the animation
+(defn load-images []
+  (if (and (:bgloaded @options) (:loaded @bird-sprite))
+    (do (add-birds!)
+        (r/render [catnip-ui] (.getElementById js/document "app"))
+        (. js/window (requestAnimationFrame update!)))
+    (js/setTimeout load-images 16)))
+
 ;; -------------------------
 ;; Initialize app
 
@@ -202,10 +248,10 @@
 ;; Sets up the initial state of the program and then kicks off the animation loop
 (defn mount-root []
   (do
-    (set-bg! "bg/nature-forest-waterfall-jungle.jpg")
-    (add-birds!)
-    (r/render [catnip-ui] (.getElementById js/document "app"))
-    (. js/window (requestAnimationFrame update!))))
+    (r/render [splash-ui] (.getElementById js/document "app"))
+    (load-images)))
+
+
 
 ;; [] -> []
 ;; This is inistial entry point when called by the web browser and just calls mount-root
---
+++ src/catnip/fullscreen.cljs
@@ -0,0 +1,37 @@
+(ns catnip.fullscreen)
+
+(defn document-prop [name]
+  (aget js/document name))
+
+(defn is-fullscreen? []
+  (let [options ["fullscreenElement"
+                 "mozFullScreenElement"
+                 "webkitFullscreenElement"
+                 "webkitCurrentFullScreenElement"
+                 "msFullscreenElement"]]
+    (some (comp boolean document-prop) options)))
+
+(defn request [dom-node]
+  (let [options ["requestFullscreen"
+                 "webkitRequestFullscreen"
+                 "webkitRequestFullScreen"
+                 "mozRequestFullScreen"
+                 "msRequestFullscreen"]
+        f (some (partial aget dom-node) options)]
+    (if f
+      (.call f dom-node))))
+
+(defn exit []
+  (let [options ["exitFullscreen"
+                 "webkitExitFullscreen"
+                 "webkitCancelFullScreen"
+                 "mozCancelFullScreen"
+                 "msExitFullscreen"]
+        f (some document-prop options)]
+    (if f
+      (.call f js/document))))
+
+(defn toggle [dom-node]
+  (if (is-fullscreen?)
+    (exit)
+    (request dom-node)))
